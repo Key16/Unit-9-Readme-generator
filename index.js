24bc767af112a1d74fcc7aa0d6f23d3ad76a93ff
Key <jh.yield@gmail.com>
2022-04-16 13:28:16 +1000

    created init and first line of the generatereadme file

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        }
+    ])
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
 
-// TODO: Create a function to initialize app
-function init() { }
+const generateREADME = ({ title }) =>
 
-// Function call to initialize app
+    `# ${title}`;
+
+
+
+const init = () => {
+    questions()
+        // Use writeFileSync method to use promises instead of a callback function
+        .then((answers) => fs.writeFileSync('README.md', generateREADME(answers)))
+        .then(() => console.log('Successfully wrote to README.md'))
+        .catch((err) => console.error(err));
+};
+
 init();
